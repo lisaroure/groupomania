88de8a53f837c3d06ace4e3ad0e14744102a208e
Lisa <lisaroure@gmail.com>
2022-07-15 18:56:21 +0200

    ajout des middlewares

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const user = require('../models/user');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        req.auth = { userId: userId };
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({
+            error: error | 'Requête non-authentifiée !'
+        })
+    }
+};
