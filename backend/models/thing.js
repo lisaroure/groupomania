66290bb51c2c6fc7461732d32ec2aeaff1f93108
Lisa <lisaroure@gmail.com>
2022-07-15 18:32:40 +0200

    ajout des premiers schéma

---
+++ backend/models/thing.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const thingSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    usersLiked: { type: [String] },
+    usersDisliked: { type: [String] },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ require('./config/db');
 const app = express();
 
 app.listen(process.env.PORT, () => {
-    console.log('Listening on port ${process.env.PORT}');
+    console.log(`Listening on port ${process.env.PORT}`);
 })
