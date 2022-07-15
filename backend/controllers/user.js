0299cefea3a59b69f6a09f858f24602b7474dac4
Lisa <lisaroure@gmail.com>
2022-07-15 19:06:26 +0200

    edit

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,6 +8,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
+                pseudo: req.body.pseudo,
                 email: req.body.email,
                 password: hash
             });
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express =  require('express');
+const userRoutes = require('./routes/user');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express();
 
+
+
+
+
+//Router
+app.use('/api/user', userRoutes);
+
+//Server 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
