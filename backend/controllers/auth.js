d968239296e1f6c2fe70df457ee90107c119f3d7
Lisa <lisaroure@gmail.com>
2022-07-16 12:44:59 +0200

    ajout cookie-parser

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,17 @@
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+const maxAge = 3 * 24 * 60 * 60 * 1000;
 
-exports.signUp = (req, res, next) => {
-    console.log("signup!")
+exports.signUp = async (req, res, next) => {
+    const { pseudo, email, password } = req.body
+    try {
+        const user = await User.create({ pseudo, email, password });
+        res.status(201).json({ user: user._id })
+    } catch (err) {
+        res.status(200).send({ err })
+    }
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
@@ -12,39 +19,43 @@ exports.signUp = (req, res, next) => {
                 email: req.body.email,
                 password: hash
             });
-            console.log(User)
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json({ error })
-
         });
 }
-exports.signIn = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: 'Login/Mot de passe incorrects' });
-            }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: 'Login/Mot de passe incorrects' });
-                    }
-                    console.log('login')
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-};
+exports.signIn = async (req, res, next) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge })
+        res.status(200).json({ user: user._id })
+    }
+    catch (err) {
+        res.status(200).json(err)
+    }
+    then(user => {
+        if (!user) {
+            return res.status(401).json({ error: 'Login/Mot de passe incorrects' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ error: 'Login/Mot de passe incorrects' });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        { userId: user._id },
+                        process.env.TOKEN_SECRET,
+                        { expiresIn: maxAge }
+                    )
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+   
+    exports.logout = (req, res, next) => {
+        res.cookie('jwt', '', {maxAge: 1 });
+        res.redirect('/');
+    }
+}
