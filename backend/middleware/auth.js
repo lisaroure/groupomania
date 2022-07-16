46098912243c933a6255c7c266959db02b7c8bf4
Lisa <lisaroure@gmail.com>
2022-07-16 13:43:14 +0200

    jwt update

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,10 +6,10 @@ module.exports.checkUser = (req, res, next) => {
         const token = req.cookies.jwt.split(' ')[1];
         const decodedToken = jwt;
         if (token) {
-            jwt.verify(token, process.env.TOKEN_SECRET, async(err, decodedToken) => {
+            jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
                 if (err) {
                     res.locals.user = null;
-                    res.cookie('jwt', '', {maxAge: 1 });
+                    res.cookie('jwt', '', { maxAge: 1 });
                     next();
                 } else {
                     let user = await User.findById(decodedToken.id);
@@ -26,5 +26,21 @@ module.exports.checkUser = (req, res, next) => {
         res.status(401).json({
             error: error | 'Requête non-authentifiée !'
         })
+    }
+};
+
+module.exports.requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(decodedToken.id);
+                next();
+            }
+        });
+    } else {
+        console.log('No token');
     }
 };
