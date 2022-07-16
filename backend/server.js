ef4a31cef7a3243bc47f87f7675140baa391dcbb
Lisa <lisaroure@gmail.com>
2022-07-16 12:45:10 +0200

    ajout cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/user');
+//const userRoutes = require('./routes/user');
 require('dotenv').config({ path: './config/.env' });
 require('./config/db');
+const {checkUser} = require('./middleware/auth');
 const app = express();
 
 // Traiter la data en transit d'un point A à un point B
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser());
 
-
+// JWT
+app.get('*', checkUser);
 
 //Routes
-app.use('/api/user', userRoutes);
+//app.use('/api/user', userRoutes);
 
 //Server 
 app.listen(process.env.PORT, () => {
