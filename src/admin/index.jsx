3b3def5c97a1d5cfe0865eb767696f16699b347d
Lili <lisaroure@gmail.com>
2022-07-17 23:45:58 +0200

    ajout index admin

---
+++ src/admin/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+//import { BrowserRouter as Route, userRouteMatch } from "react-router-dom"
+//import Dashboard from './components/Dashboard'
+//import Settings from './components/Settings'
+
+function Admin () {
+    return (
+        <div>
+            <h1>Espace Admin</h1>
+        </div>
+    )
+}
+
+export default Admin
