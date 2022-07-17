6b39b163f0d64d11b74587aafa5be9c5f47504a1
Lili <lisaroure@gmail.com>
2022-07-17 23:45:49 +0200

    ajout de l'index user

---
+++ src/user/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+//import { BrowserRouter as Router, Route } from "react-router-dom"
+
+
+function User () {
+    return (
+        <div>
+            <h1>Espace Admin</h1>
+        </div>
+    )
+}
+
+export default User
