1ebe518d8565a4040e1ac8807ddda7caf7582a12
Lili <lisaroure@gmail.com>
2022-07-17 23:45:34 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './components/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import User from './user'
+import Admin from './admin'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+       <Router>
+        <h2>Page principale à la racine</h2>
+           <switch>
+            <Route path="/admin">
+                <Admin />
+                </Route>
+                <Route path='/user'>
+                    <User/>
+                </Route>
+               </switch>
+               </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
